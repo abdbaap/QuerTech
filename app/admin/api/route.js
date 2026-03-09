c25e4fa2934c7d28d8088cca7b86abc7f04678cb
abdbaap <soniaajmani6@gmail.com>
2026-03-09 12:56:00 +0530

    ddfdf

--- app/admin/api/route.js
+++ app/admin/api/route.js
@@ -8,7 +8,7 @@ export async function POST(req) {
         body:JSON.stringify({
             host:"quertech-articles.vercel.app",
             key:process.env.INDEXNOWKEY,
-            keyLocation:`https://quertech-articles.vercel.app/${process.env.INDEXNOWKEY}.txtdf`,
+            keyLocation:`https://quertech-articles.vercel.app/${process.env.INDEXNOWKEY}.txt`,
             urlList:urlList
                 })
     })
