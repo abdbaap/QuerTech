85a30583137735aa467965fb320d7f3f2219f0ba
Abd Workspace <soniaajmani6@gmail.com>
2026-04-12 15:43:15 +0530

    Update route.js

--- app/admin/apiforOtherProcess/route.js
+++ app/admin/apiforOtherProcess/route.js
@@ -1,6 +1,4 @@
 // app/api/publish/route.ts
-export const runtime = 'edge'; 
-export const dynamic = 'force-dynamic';
 
 import { NextResponse } from 'next/server';
 
@@ -9,21 +7,15 @@ export async function POST(request,response) {
     
 
     // 1. Check if we are running locally or in production
-    const isLocal = process.env.NODE_ENV === 'development';
-
-    if (isLocal) {
-        try {
+    
             // 2. ONLY import the library if we are local.
             // This prevents Cloudflare from crashing during the build/deploy.
             const { publishToQueue } = await import('@/app/utils/rabbitmq');
             
             await publishToQueue("blog.generated", {url:data.url,summary:data.summary});
             return NextResponse.json({ success: true, message: "Queued locally" });
-        } catch (error) {
-            console.error("Local RabbitMQ Error:", error);
-            return NextResponse.json({ success: false, error: "Local queue failed" }, { status: 500 });
-        }
-    }
+         
+    
 
     // 3. Fallback for Cloudflare (Production)
     // You can either return a fake success or a message saying it's disabled.
