02830ff6b6f59e4e869266fd39ad7d908b4a287a
Abd Workspace <soniaajmani6@gmail.com>
2026-04-12 16:06:36 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
     {/* <meta name="monetag" content="818a149133551c7c42aaf1e1294321aa"/> */}
-        <meta name="google-adsense-account" content="ca-pub-6836466532865002">
+        <meta name="google-adsense-account" content="ca-pub-6836466532865002"/>
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6836466532865002"
      crossorigin="anonymous"></script>
        {/* <script src="https://quge5.com/88/tag.min.js" data-zone="228625" async data-cfasync="false"></script> */}
@@ -24,12 +24,7 @@ export default function RootLayout({ children }) {
        <meta name="google-site-verification" content="7GU0yZN31fEJZ6ejGBoKIWqkDuVLHyuFJHA485bWWE4" />
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6836466532865002"
      crossorigin="anonymous"></script>
-       <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6836466532865002"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+       
       </head>
       <body
         className={`${poppins.className} noscrollbar`}
